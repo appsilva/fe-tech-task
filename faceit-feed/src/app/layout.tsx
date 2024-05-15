53aea8988bf369f03410255e415810a1b57cd77a
António Silva <antonio_silva9@live.com.pt>
2024-05-15 15:56:22 +0100

    chore: bootstrap app

---
+++ faceit-feed/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "FACEIT Feed App",
+  description: "Feed App",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
